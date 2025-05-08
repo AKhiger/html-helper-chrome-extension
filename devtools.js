7a9f2ed4368a91caf84b96fccc0b975b7730d679
AKhiger <xgr.temp.il@gmail.com>
2025-05-08 20:51:56 +0300

    test

--- devtools.js
+++ devtools.js
@@ -3,4 +3,4 @@
 // chrome.extension.*
 
 // Create a tab in the devtools area
-chrome.devtools.panels.create("HTML Helper", "html.png", "panel.html", function(panel) {});
+chrome.devtools.panels.create("HTML Helper ", "html.png", "panel.html", function(panel) {});
